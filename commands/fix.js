a40516d8ef9fe2527643afa3f7d26b446c897d4b
Florian Kinder <florian.kinder@mcl.de>
2020-05-26 14:31:21 +0200

    Added fix command

---
+++ commands/fix.js
@@ -0,0 +1,25 @@
+const Guild = require('../database/guild');
+const HighSellAnnouncement = require('../database/highsell-announcement');
+
+module.exports = {
+	name: 'fix',
+	description: 'Fix bot configuration',
+	guildOnly: true,
+	cooldown: 30,
+	hidden: true,
+	async execute(message) {
+		try {
+			await Guild.findOne({ where: { guild_id: message.channel.guild.id } });
+		}
+		catch (e) {
+			await Guild.create({
+				guild_id: message.channel.guild.id,
+			});
+			await HighSellAnnouncement.destroy({ where: {
+				guild_id: message.channel.guild.id,
+			} });
+		}
+
+		await message.react(':white_check_mark:');
+	},
+};
