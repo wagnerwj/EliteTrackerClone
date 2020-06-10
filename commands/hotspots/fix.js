be60e7621fe02b214b06e74a5dea9727015fc5dc
Florian Kinder <florian.kinder@mcl.de>
2020-06-10 16:50:00 +0200

    Added fix command

---
+++ commands/hotspots/fix.js
@@ -0,0 +1,29 @@
+const Hotspot = require('../../database/hotspot');
+const { allowedCommodities } = require('./data');
+
+module.exports = {
+	name: 'fix',
+	description: 'Fix data',
+	cooldown: 10,
+	hidden: true,
+	owner: true,
+	async execute(message) {
+		let changes = 0;
+		const hotspots = await Hotspot.findAll({});
+		for (const hotspot of hotspots) {
+			const commodity = allowedCommodities.find((c) => c.toLowerCase() === hotspot.commodity.toLowerCase());
+			if (commodity && commodity !== hotspot.commodity) {
+				const affectedRows = await Hotspot.update({
+					commodity: commodity,
+				}, {
+					where: {
+						id: hotspot.id,
+					},
+				});
+				changes += affectedRows[0];
+			}
+		}
+
+		return message.channel.send(`${changes} entries updated`);
+	},
+};
