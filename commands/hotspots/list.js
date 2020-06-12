217754df51516365d64452f8a61c1cbe8d5f7e3d
Florian Kinder <florian.kinder@mcl.de>
2020-06-12 14:04:15 +0200

    Replaced the hotspots list command

--- commands/hotspots/list.js
+++ commands/hotspots/list.js
@@ -1,49 +1,20 @@
 const { prefix } = require(process.env.CONFIG_PATH || '../../config.json');
-const Hotspot = require('../../database/hotspot');
-const HotspotAdmin = require('../../database/hotspot-admin');
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
 
 module.exports = {
 	name: 'list',
-	description: 'List all reported hotspots',
-	usage: '[unapproved, to show unapproved hotspots]',
-	async execute(message, args) {
-		const admin = await HotspotAdmin.findOne({ where: { adminID: message.author.id } });
+	description: 'Show commands to find hotspots',
+	async execute(message) {
+		return message.channel.send(`**Show hotspots statistic**:
+\`${prefix}hotspots stats\`
 
-		let filter = {};
-		let unapproved = false;
-		for (const arg of args) {
-			if (arg === 'unapproved') {
-				unapproved = true;
-			}
-		}
-		if (unapproved) {
-			filter['approver_id'] = null;
-		}
-		else {
-			filter = {
-				approver_id: {
-					[Op.ne]: null,
-				},
-			};
-		}
+**Show hotspots in a system**:
+\`${prefix}hotspots system [system name]\`
 
-		let text = '';
-		const hotspots = await Hotspot.findAll(filter ? { where: filter } : undefined);
-		for (const hotspot of hotspots) {
-			text += `${!hotspot.approver_id && admin ? `> Approval:\n\`${prefix}hotspots approve ${hotspot.id}\`\n\`${prefix}hotspots decline ${hotspot.id}\`\n\n` : ''}Location **${hotspot.body_name}**
-Commodity **${hotspot.commodity} x${hotspot.overlaps}**
-> Reported at ${hotspot.createdAt.toUTCString()} from ${hotspot.reporter}:
-${hotspot.description}
+**Show hotspots location for a given commodity**:
+\`${prefix}hotspots locations [commodity]\`
 
-`;
-		}
-
-		if (!text) {
-			return message.channel.send('No hotspots found');
-		}
-
-		return message.channel.send(text, { split: true });
+**Show all unapproved hotspots reports**:
+\`${prefix}hotspots unapproved\`
+`);
 	},
 };
---
+++ commands/hotspots/unapproved.js
@@ -0,0 +1,28 @@
+const { prefix } = require(process.env.CONFIG_PATH || '../../config.json');
+const Hotspot = require('../../database/hotspot');
+const HotspotAdmin = require('../../database/hotspot-admin');
+
+module.exports = {
+	name: 'unapproved',
+	description: 'List unapproved hotspots',
+	async execute(message) {
+		const admin = await HotspotAdmin.findOne({ where: { adminID: message.author.id } });
+
+		let text = '';
+		const hotspots = await Hotspot.findAll({ where: { approver_id: null } });
+		for (const hotspot of hotspots) {
+			text += `${!hotspot.approver_id && admin ? `> Approval:\n\`${prefix}hotspots approve ${hotspot.id}\`\n\`${prefix}hotspots decline ${hotspot.id}\`\n\n` : ''}Location **${hotspot.body_name}**
+Commodity **${hotspot.commodity} x${hotspot.overlaps}**
+> Reported at ${hotspot.createdAt.toUTCString()} from ${hotspot.reporter}:
+${hotspot.description}
+
+`;
+		}
+
+		if (!text) {
+			return message.channel.send('No hotspots found');
+		}
+
+		return message.channel.send(text, { split: true });
+	},
+};
